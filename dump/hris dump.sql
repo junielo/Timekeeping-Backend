9c91ee229c6e3787deb6287067d959c3745c5834
Junielo Guardalupe <jun.guardalupe@gmail.com>
2019-12-07 11:21:41 +0800

    new schema

--- dump/hris dump.sql
+++ dump/hris dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hris_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `hris_db`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: hris_db
@@ -168,4 +170,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-07 11:17:41
+-- Dump completed on 2019-12-07 11:21:10
